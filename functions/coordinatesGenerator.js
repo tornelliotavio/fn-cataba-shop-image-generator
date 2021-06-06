db24670aab2fa11350bca51db6555aa431c5ecfd
Otavio Tornelli Duarte <tornelli.otavio20@gmail.com>
2021-06-06 16:50:13 -0300

    Started coordinates generator 2

--- functions/coordinatesGenerator.js
+++ functions/coordinatesGenerator.js
@@ -5,22 +5,37 @@ const requests = require("./requests");
 const sortShopEntries = require("./sortShopEntries");
 
 async function coordinatesGenerator() {
-
   let sortedShopEntries = await sortShopEntries();
 
-    //coordinate generator settings
-    const tileSize = {
-        DoubleWide:{
+  //coordinate generator settings
+  let baseHeight = 248;
 
-        }
-    }
+  const tileSize = {
+    DoubleWide: {
+      widthMultiplier: 1.2,
+      heightMultiplier: 1,
+    },
+    Normal: {
+      widthMultiplier: 0.58,
+      heightMultiplier: 1,
+    },
+    Small: {
+      widthMultiplier: 0.58,
+      heightMultiplier: 0.477,
+    },
+  };
 
-    for(let entryIndex in sortedShopEntries){
+  for (let entryIndex in sortedShopEntries) {
+      
+      sortedShopEntries[entryIndex].size = {}
+      sortedShopEntries[entryIndex].size.width = Math.round(tileSize[sortedShopEntries[entryIndex].tileSize].widthMultiplier*baseHeight)
+      sortedShopEntries[entryIndex].size.height = Math.round(tileSize[sortedShopEntries[entryIndex].tileSize].heightMultiplier*baseHeight)
 
-        console.log(entryIndex)
+      let entry = sortedShopEntries[entryIndex]
 
-    }
+    console.log(entry)
 
+  }
 }
 
 module.exports = coordinatesGenerator;
